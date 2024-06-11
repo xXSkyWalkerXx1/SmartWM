4fc0580783eba3e36558ebc4c4eb447000c90e46
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-06-11 17:29:21 +0200

    added param SecuritiesType to method extract

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/scraping/management/extraction/Extraction.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/scraping/management/extraction/Extraction.java
@@ -1,9 +1,10 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.scraping.management.extraction;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.historic.data.SecuritiesType;
 import de.tud.inf.mmt.wmscrape.gui.tabs.scraping.data.element.WebsiteElement;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.concurrent.Task;
 
 public interface Extraction {
-    void extract(WebsiteElement element, Task<Void> task, SimpleDoubleProperty progress);
+    void extract(SecuritiesType securitiesType, WebsiteElement element, Task<Void> task, SimpleDoubleProperty progress);
 }
