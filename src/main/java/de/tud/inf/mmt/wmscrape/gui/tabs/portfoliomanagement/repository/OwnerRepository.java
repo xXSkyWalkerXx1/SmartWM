3cc282dfa53c8b732cce5df35608cd35939bd0b4
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-10-29 18:19:36 +0100

    added method getAll

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/OwnerRepository.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/OwnerRepository.java
@@ -3,5 +3,9 @@ package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    List<Owner> getAll();
 }
