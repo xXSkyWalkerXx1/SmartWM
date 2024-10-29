4f06e40c597311a9938196d720758b1c5f02daea
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-10-29 17:48:38 +0100

    first commit

---
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/entity/Address.java
@@ -0,0 +1,71 @@
+package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "owner_adress")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    @Column(name = "plz", nullable = false)
+    private String plz;
+
+    @Column(name = "location", nullable = false)
+    private String location; // Stadt/Dorf
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "street_number", nullable = false)
+    private String streetNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+}
---
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/enums/MaritalState.java
@@ -0,0 +1,21 @@
+package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums;
+
+public enum MaritalState {
+    SINGLE("Ledig"),
+    MARRIED("Verheiratet"),
+    WIDOWED("Verwitwet"),
+    DIVORCED("Geschieden"),
+    REGISTERED_CIVIL_PARTNERSHIP("Registrierte Lebenspartnerschaft"),
+    REGISTERED_LIFE_PARTNER_DECEASED("Eingetragene/r Lebenspartner/in verstorben"),
+    REGISTERED_CIVIL_PARTNERSHIP_ANNULLED("Eingetragene Lebenspartnerschaft aufgehoben");
+
+    private final String displayText;
+
+    MaritalState(String displayText){
+        this.displayText = displayText;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
+}
