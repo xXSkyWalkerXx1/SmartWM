68a18ddfe2543b4f6b58248a8f486bf9c4677509
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-10-30 16:51:38 +0100

    implemented missing columns

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/entity/Depot.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/entity/Depot.java
@@ -12,21 +12,61 @@ import java.util.List;
 @Table(name = "depot")
 public class Depot implements Valuable {
 
+    // region Entity as inner-class
+    @Entity
+    @Table(name = "depot_bank")
+    public static class DepotBank {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(name = "name", nullable = false)
+        private String name; // bank-name
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    // endregion
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
 
     @OneToOne
     @JoinColumn(name = "owner_id", nullable = false)
     private Owner owner;
 
+    @OneToOne
+    @JoinColumn(name = "bank_id", nullable = false)
+    private DepotBank bank;
+
+    @OneToOne
+    @JoinColumn(name = "portfolio_id", nullable = false)
+    private Portfolio portfolio;
+
+    @Column(name = "notice")
+    private String notice;
+
     @OneToMany
     @JoinColumn(name = "billing_account_id", nullable = false)
-    private List<Account> billingAccounts = new ArrayList<>();
+    private List<Account> billingAccounts = new ArrayList<>(); // Verrechnungskonten
 
     @OneToOne
     @JoinColumn(name = "commission_scheme_id", nullable = false)
-    private CommissionScheme commissionScheme;
+    private CommissionScheme commissionScheme; // Provision-schema
 
     // region Getters & Setters
     @Override
@@ -38,12 +78,44 @@ public class Depot implements Valuable {
         return id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Owner getOwner() {
         return owner;
     }
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public DepotBank getBank() {
+        return bank;
+    }
+
+    public void setBank(DepotBank bank) {
+        this.bank = bank;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice;
     }
 
     public List<Account> getBillingAccounts() {
