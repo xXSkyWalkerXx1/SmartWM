c12b231c73ffe4063dcdd95624cd15e65d803939
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-10-30 13:05:24 +0100

    added all missing attributes/columns

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/entity/Portfolio.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/entity/Portfolio.java
@@ -1,6 +1,10 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums.State;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "portfolio")
@@ -10,7 +14,32 @@ public class Portfolio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false)
+    private String name;
+
     @ManyToOne
-    @JoinColumn(name = "owner")
+    @JoinColumn(name = "owner_id", nullable = false)
     private Owner owner;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false)
+    private State state;
+
+    @Column(name = "created_at", nullable = false)
+    private String createdAt;
+
+    @Column(name = "deactivated_at")
+    private String deactivatedAt;
+
+    @OneToOne
+    @JoinColumn(name = "investment_guideline_id", nullable = false)
+    private InvestmentGuideline investmentGuideline;
+
+    @OneToMany
+    @JoinColumn(name = "depot_id", nullable = false)
+    private List<Depot> depots = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn(name = "account_id", nullable = false)
+    private List<Account> accounts = new ArrayList<>();
 }
