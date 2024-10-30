b5d3a91c982528eb9bb17544a9040095dffdc655
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-10-30 13:03:45 +0100

    added owner, billingAccounts, commissionScheme

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/entity/Depot.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/entity/Depot.java
@@ -1,6 +1,8 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "depot")
@@ -9,4 +11,46 @@ public class Depot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private Owner owner;
+
+    @OneToMany
+    @JoinColumn(name = "billing_account_id", nullable = false)
+    private List<Account> billingAccounts = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "commission_scheme_id", nullable = false)
+    private CommissionScheme commissionScheme;
+
+    // region Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public List<Account> getBillingAccounts() {
+        return billingAccounts;
+    }
+
+    public void setBillingAccounts(List<Account> billingAccounts) {
+        this.billingAccounts = billingAccounts;
+    }
+
+    public CommissionScheme getCommissionScheme() {
+        return commissionScheme;
+    }
+
+    public void setCommissionScheme(CommissionScheme commissionScheme) {
+        this.commissionScheme = commissionScheme;
+    }
+    // endregion
 }
