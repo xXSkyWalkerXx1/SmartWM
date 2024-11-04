7932aeb50447ad145ea4a1bd4c135630f6f154e8
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-04 11:12:31 +0100

    first commit

---
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/OwnerService.java
@@ -0,0 +1,19 @@
+package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners;
+
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository.OwnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OwnerService {
+
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    public List<Owner> getAllOwners(){
+        return ownerRepository.findAll();
+    }
+}
