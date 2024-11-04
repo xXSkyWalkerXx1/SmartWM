8170fd14139be6d990764bcbd0f80562869b29af
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-04 10:44:20 +0100

    changed CrudRepository to JpaRepository

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/OwnerRepository.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/OwnerRepository.java
@@ -1,13 +1,14 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository;
 
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface OwnerRepository extends CrudRepository<Owner, Integer> {
+public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 
     List<Owner> findAll();
 }
