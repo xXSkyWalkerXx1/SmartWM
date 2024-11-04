92419a4c186b0ff67825c2296e2b7a20ac78b28d
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-04 15:30:18 +0100

    implemented method onClickCreateOwner and setOwnerTreeView

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/OwnerController.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/OwnerController.java
@@ -1,37 +1,60 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.PrimaryTabController;
+import de.tud.inf.mmt.wmscrape.gui.tabs.PrimaryTabManager;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.PortfolioManagementTabManager;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.PortfolioTreeView;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.TableFactory;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
 
 @Controller
 public class OwnerController {
 
+    @Autowired
+    private OwnerService ownerService;
+    @Autowired
+    private CreateOwnerDialog createOwnerDialog; // controller
+
     @FXML
-    PortfolioManagementTabManager portfolioManagementTabManager;
+    Button createOwnerButton;
     @FXML
     AnchorPane ownerTablePane;
     @FXML
-    TreeView<Owner> portfoliosTreeView;
-
-    @Autowired
-    public OwnerController(PortfolioManagementTabManager portfolioManagementTabManager) {
-        this.portfolioManagementTabManager = portfolioManagementTabManager;
-    }
+    AnchorPane ownerTreeViewPane;
 
     @FXML
     private void initialize() {
-        ownerTablePane.getChildren().add(TableFactory.createOwnerTable(ownerTablePane, new ArrayList<>()));
+        ownerTablePane.getChildren().add(TableFactory.createOwnerTable(
+                ownerTablePane,
+                ownerService.getAllOwners(),
+                this
+        ));
+    }
+
+    @FXML
+    public void onClickCreateOwner(){
+        PrimaryTabManager.loadFxml(
+                "tabs/portfoliomanagement/tab/owners/createOwnerDialog.fxml",
+                "Neuen Inhaber anlegen",
+                null,
+                true,
+                createOwnerDialog,
+                false
+        );
+    }
+
+    public void setOwnerTreeView(@NonNull PortfolioTreeView portfolioTreeView){
+        ownerTreeViewPane.getChildren().clear();
+        ownerTreeViewPane.getChildren().add(portfolioTreeView);
     }
 
 }
