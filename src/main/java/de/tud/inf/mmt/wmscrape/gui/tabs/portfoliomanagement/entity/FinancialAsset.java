70f4685c53e1264c0faf07fda4dfce37eeb7ab74
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-06 16:36:20 +0100

    initial commit

---
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/entity/FinancialAsset.java
@@ -0,0 +1,6 @@
+package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity;
+
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.interfaces.Valuable;
+
+public abstract class FinancialAsset implements Valuable {
+}
