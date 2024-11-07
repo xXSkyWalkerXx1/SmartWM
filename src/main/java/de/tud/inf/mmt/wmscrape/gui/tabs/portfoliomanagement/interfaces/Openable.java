f6c8b4a9c661204a4416b41809e35fb3d3f2fb6a
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-07 21:41:22 +0100

    removed generic and param on method open

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/interfaces/Openable.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/interfaces/Openable.java
@@ -4,8 +4,7 @@ package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.interfaces;
  * Indicates a controller (or in detail his connected view) that he is used to loading a specific entity.
  * F.e.: {@link de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners.owner.OwnerOverviewController} loads a
  * specific {@link de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner} entity.
- * @param <T> Entity to load.
  */
-public interface Openable<T> {
-    void open(T entity);
+public interface Openable {
+    void open();
 }
