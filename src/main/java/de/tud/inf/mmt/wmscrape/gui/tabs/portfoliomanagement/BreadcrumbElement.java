71caba7b976e74ab397930e898790c55418e61aa
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-07 21:52:42 +0100

    removed method getId

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/BreadcrumbElement.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/BreadcrumbElement.java
@@ -1,9 +1,5 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement;
 
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class BreadcrumbElement{
     public String element;
     public String type;
@@ -11,18 +7,6 @@ public class BreadcrumbElement{
     public BreadcrumbElement(String element, String type) {
         this.element = element;
         this.type = type;
-    }
-
-    /**
-     * @return The id as String, if found in {@link BreadcrumbElement#element} (if this element has a format like 'Inhaber / Max Mustermann (ID: id)').
-     * @throws IllegalArgumentException if it could not find id.
-     */
-    public String getId() {
-        Pattern regexIdPattern = Pattern.compile("\\\\(ID: (.*?)\\\\)");
-        Matcher patternMatcher = regexIdPattern.matcher(element);
-
-        if (patternMatcher.find()) return patternMatcher.group(1);
-        throw new IllegalArgumentException("Id konnte nicht gefunden werden!");
     }
 }
 
