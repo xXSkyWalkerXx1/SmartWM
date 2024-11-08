5c9992a6e3a3280c47744b44c6fbaace9b3d9e6c
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-08 10:55:49 +0100

    first commit

---
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/Navigator.java
@@ -0,0 +1,46 @@
+package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement;
+
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Account;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Depot;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Portfolio;
+import org.springframework.lang.NonNull;
+
+public class Navigator {
+
+    /**
+     * Navigates to overview of specific owner.
+     */
+    public static void navigateToOwner(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
+                                       @NonNull Owner owner) {
+        portfolioManagementTabManager.showInhaberTabs(owner);
+        portfolioManagementTabManager.getPortfolioController().getOwnerOverviewController().open();
+    }
+
+    /**
+     * Navigates to overview of specific portfolio.
+     */
+    public static void navigateToPortfolio(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
+                                           @NonNull Portfolio portfolio) {
+        portfolioManagementTabManager.showPortfolioTabs();
+        // ToDo: portfolioManagementTabManager.getPortfolioController().getPortfolioOverviewController().open()
+    }
+
+    /**
+     * Navigates to overview of specific account.
+     */
+    public static void navigateToAccount(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
+                                         @NonNull Account account) {
+        portfolioManagementTabManager.showKontoTabs(account);
+        portfolioManagementTabManager.getPortfolioController().getKontoOverviewController().open();
+    }
+
+    /**
+     * Navigates to overview of specific depot.
+     */
+    public static void navigateToDepot(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
+                                       @NonNull Depot depot) {
+        portfolioManagementTabManager.showDepotTabs();
+        // ToDo: portfolioManagementTabManager.getPortfolioController().getDepotOverviewController().open()
+    }
+}
