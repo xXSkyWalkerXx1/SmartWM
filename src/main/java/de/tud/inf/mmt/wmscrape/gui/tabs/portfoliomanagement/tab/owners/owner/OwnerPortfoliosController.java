3375b218467469164ff4fe34d87ea30620c9bec4
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-08 10:42:53 +0100

    fully implemented

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/owner/OwnerPortfoliosController.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/owner/OwnerPortfoliosController.java
@@ -1,18 +1,64 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners.owner;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.PortfolioManagementTabController;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.PortfolioManagementTabManager;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Account;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Depot;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.interfaces.Openable;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.TableFactory;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class OwnerPortfoliosController {
+public class OwnerPortfoliosController implements Openable {
 
-    @FXML
-    PortfolioManagementTabManager portfolioManagementTabManager;
+    Owner owner;
 
     @Autowired
-    public OwnerPortfoliosController(PortfolioManagementTabManager portfolioManagementTabManager) {
-        this.portfolioManagementTabManager = portfolioManagementTabManager;
+    PortfolioManagementTabManager portfolioManagementManager;
+
+    @FXML
+    AnchorPane portfoliosTablePane;
+
+    @FXML
+    AnchorPane accountsTablePane;
+
+    @FXML
+    AnchorPane depotsTablePane;
+
+    @Override
+    public void open() {
+        owner = (Owner) portfolioManagementManager
+                .getPortfolioController()
+                .getInhaberPortfoliosTab()
+                .getProperties()
+                .get(PortfolioManagementTabController.TAB_PROPERTY_ENTITY);
+
+        portfoliosTablePane.getChildren().add(TableFactory.createOwnerPortfoliosTable(
+                portfoliosTablePane,
+                accountsTablePane,
+                depotsTablePane,
+                owner.getPortfolios(),
+                this
+        ));
+    }
+
+    public void setAccountTable(TableView<Account> table) {
+        accountsTablePane.getChildren().clear();
+        accountsTablePane.getChildren().add(table);
+    }
+
+    public void setDepotTable(TableView<Depot> table) {
+        depotsTablePane.getChildren().clear();
+        depotsTablePane.getChildren().add(table);
+    }
+
+    public PortfolioManagementTabManager getPortfolioManagementManager() {
+        return portfolioManagementManager;
     }
 }
