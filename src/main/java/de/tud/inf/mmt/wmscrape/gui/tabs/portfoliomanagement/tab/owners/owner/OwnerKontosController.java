d463b0edf6e2e22754e22ea5be903ce744c86714
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-08 15:06:02 +0100

    fully implemented

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/owner/OwnerKontosController.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/owner/OwnerKontosController.java
@@ -1,18 +1,42 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners.owner;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.PortfolioManagementTabController;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.PortfolioManagementTabManager;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.interfaces.Openable;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.TableFactory;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class OwnerKontosController {
+public class OwnerKontosController implements Openable {
 
-    @FXML
-    PortfolioManagementTabManager portfolioManagementTabManager;
+    Owner owner;
 
     @Autowired
-    public OwnerKontosController(PortfolioManagementTabManager portfolioManagementTabManager) {
-        this.portfolioManagementTabManager = portfolioManagementTabManager;
+    PortfolioManagementTabManager portfolioManagementTabManager;
+
+    @FXML
+    AnchorPane accountsTablePane;
+
+    @Override
+    public void open() {
+        owner = (Owner) portfolioManagementTabManager
+                .getPortfolioController()
+                .getInhaberKontosTab()
+                .getProperties()
+                .get(PortfolioManagementTabController.TAB_PROPERTY_ENTITY);
+
+        accountsTablePane.getChildren().add(TableFactory.createOwnerAccountsTable(
+                accountsTablePane,
+                owner.getAccounts().stream().toList(),
+                this
+        ));
+    }
+
+    public PortfolioManagementTabManager getPortfolioManagementManager() {
+        return portfolioManagementTabManager;
     }
 }
