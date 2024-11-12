83ec94092ec82f7db5cf225c52be6d30689aa8dd
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-12 14:06:58 +0100

    implemented method getAll

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/PortfolioService.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/PortfolioService.java
@@ -1,7 +1,19 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.portfolios;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Portfolio;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository.PortfolioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PortfolioService {
+
+    @Autowired
+    PortfolioRepository portfolioRepository;
+
+    public List<Portfolio> getAll() {
+        return portfolioRepository.findAll();
+    }
 }
