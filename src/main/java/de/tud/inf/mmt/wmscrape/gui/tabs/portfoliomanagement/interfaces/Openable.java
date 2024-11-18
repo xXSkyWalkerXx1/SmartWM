47c891fdb207acede5cf72efe1a6415820eaeb33
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-18 10:42:00 +0100

    added todo

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/interfaces/Openable.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/interfaces/Openable.java
@@ -1,9 +1,14 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.interfaces;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
+
 /**
- * Indicates a controller (or in detail his connected view) that he is used to loading a specific entity.
- * F.e.: {@link de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners.owner.OwnerOverviewController} loads a
- * specific {@link de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner} entity.
+ * Indicates a controller (or in detail his connected view) that he is used to load data even
+ * after initialization via {@code @FXML private void initialize() {}}.
+ *
+ * ToDo: rename to 'Loadable' with method 'load()'
+ * Indicates a tab-controller that his view uses dynamic data (f.e. {@link Owner}, so his view has to be updatable.
+ * Calling {@code load()} should be done, if a click on this tab took place.
  */
 public interface Openable {
     void open();
