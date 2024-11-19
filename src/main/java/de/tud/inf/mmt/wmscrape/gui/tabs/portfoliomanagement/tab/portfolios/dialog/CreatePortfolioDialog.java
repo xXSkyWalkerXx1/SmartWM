97164384226a8536d5c3b40178faaf52b43dc3e6
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-19 16:18:33 +0100

    fully implemented

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/dialog/CreatePortfolioDialog.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/dialog/CreatePortfolioDialog.java
@@ -1,15 +1,23 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.portfolios.dialog;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.PrimaryTabManager;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.PortfolioManagementTabManager;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.InvestmentGuideline;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Portfolio;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners.OwnerService;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.portfolios.PortfolioService;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.FieldValidator;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.InvestmentGuidelineTable;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.TableFactory;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.Calendar;
 
 @Controller
 public class CreatePortfolioDialog {
@@ -18,6 +26,10 @@ public class CreatePortfolioDialog {
 
     @Autowired
     OwnerService ownerService;
+    @Autowired
+    PortfolioService portfolioService;
+    @Autowired
+    private PortfolioManagementTabManager portfolioManagementTabManager;
 
     @FXML
     TextField inputPortfolioName;
@@ -34,9 +46,19 @@ public class CreatePortfolioDialog {
     @FXML
     AnchorPane commissionSchemeCurrencyTablePane;
 
+    CreatePortfolioDialog() {
+        portfolio.getInvestmentGuideline().initializeEntries();
+    }
+
     @FXML
     private void initialize() {
         inputOwner.getItems().addAll(ownerService.getAllOwners());
+        inputOwner.getSelectionModel().selectFirst();
+
+        commissionSchemeTablePane.getChildren().add(new InvestmentGuidelineTable(
+                commissionSchemeTablePane,
+                portfolio.getInvestmentGuideline().getEntries()
+        ));
         commissionSchemeLocationTablePane.getChildren().add(TableFactory.createPortfolioDivisionByLocationTable(
                 commissionSchemeLocationTablePane,
                 portfolio.getInvestmentGuideline().getDivisionByLocation()
@@ -48,8 +70,99 @@ public class CreatePortfolioDialog {
     }
 
     @FXML
-    private void onSave() {}
+    private void onCancel() {
+        portfolioManagementTabManager.getPortfolioController().getPortfolioListController().open();
+        inputPortfolioName.getScene().getWindow().hide();
+    }
 
     @FXML
-    private void onCancel() {}
+    private void onSave() {
+        // Validate first
+        if (FieldValidator.isInputEmpty(inputPortfolioName)) return;
+
+        float sum_ = 0;
+        for (InvestmentGuideline.Entry entry : portfolio.getInvestmentGuideline().getEntries()) {
+            if (!entry.getType().isChild()) {
+                var assetAlloc = entry.getAssetAllocation();
+                sum_ += assetAlloc;
+
+                float childsSum = 0;
+                for (InvestmentGuideline.Entry child : entry.getChildEntries()) childsSum += child.getAssetAllocation();
+                if (!entry.getChildEntries().isEmpty() && assetAlloc != 0 && childsSum != 100) {
+                    PrimaryTabManager.showDialog(
+                            Alert.AlertType.ERROR,
+                            "Fehler",
+                            String.format("Die Aufteilung des Gesamtvermögens für %s muss in Summe 100 ergeben (Ist: %s).", entry.getType(), childsSum),
+                            (TreeTableView<?>) commissionSchemeTablePane.getChildren().get(0)
+                    );
+                    return;
+                }
+            }
+        }
+        if (sum_ != 100) {
+            //commissionSchemeTablePane.getChildren().get(0).setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+            PrimaryTabManager.showDialog(
+                    Alert.AlertType.ERROR,
+                    "Fehler",
+                    String.format("Die Aufteilung des Gesamtvermögens muss in Summe 100 ergeben (Ist: %s).", sum_),
+                    (TreeTableView<?>) commissionSchemeTablePane.getChildren().get(0)
+            );
+            return;
+        }
+
+        var divByLoc = portfolio.getInvestmentGuideline().getDivisionByLocation();
+        float sumDivByLoc = sum(
+                divByLoc.getGermany(), divByLoc.getEurope_without_brd(), divByLoc.getNorthamerica_with_usa(),
+                divByLoc.getAsia_without_china(), divByLoc.getChina(), divByLoc.getJapan(), divByLoc.getEmergine_markets()
+        );
+        if (sumDivByLoc != 100) {
+            //commissionSchemeLocationTablePane.getChildren().get(0).setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+            PrimaryTabManager.showDialog(
+                    Alert.AlertType.ERROR,
+                    "Fehler",
+                    String.format("Die Aufteilung des Gesamtvermögens nach Ländern bzw. Regionen muss in Summe 100 ergeben (Ist: %s).", sumDivByLoc),
+                    (TableView<?>) commissionSchemeLocationTablePane.getChildren().get(0)
+            );
+            return;
+        }
+
+        var divByCurr = portfolio.getInvestmentGuideline().getDivisionByCurrency();
+        float sumDivByCurr = sum(
+                divByCurr.getEuro(), divByCurr.getUsd(), divByCurr.getGbp(), divByCurr.getYen(),
+                divByCurr.getAsiaCurrencies(), divByCurr.getOthers()
+        );
+        if (sumDivByCurr != 100) {
+            //commissionSchemeCurrencyTablePane.getChildren().get(0).setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+            PrimaryTabManager.showDialog(
+                    Alert.AlertType.ERROR,
+                    "Fehler",
+                    String.format("Die Aufteilung des Gesamtvermögens nach Währung muss in Summe 100 ergeben (Ist: %s).", sumDivByCurr),
+                    (TableView<?>) commissionSchemeCurrencyTablePane.getChildren().get(0)
+            );
+            return;
+        }
+
+        // If everything is valid, we can create and save the new portfolio
+        portfolio.setName(inputPortfolioName.getText());
+        portfolio.setOwner(inputOwner.getValue());
+        portfolio.setCreatedAt(Calendar.getInstance().getTime());
+
+        portfolioService.save(portfolio);
+        onCancel();
+
+        // Finally, show success-dialog
+        PrimaryTabManager.showInfoDialog(
+                "Portfolio angelegt",
+                "Das neue Portfolio wurde erfolgreich angelegt.",
+                inputPortfolioName
+        );
+    }
+
+    private float sum(Float... attribute) {
+        float sum = 0;
+        for (Float value : attribute) {
+            sum += value;
+        }
+        return sum;
+    }
 }
