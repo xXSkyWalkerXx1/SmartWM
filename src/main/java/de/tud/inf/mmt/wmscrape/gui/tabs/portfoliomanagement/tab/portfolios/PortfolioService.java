32ef88c4af507ef8e8e21149bd2ca6efb80bb917
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-20 17:22:02 +0100

    implemented methods: savePortfolio, isPortfolioInputInvalid, sum

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/PortfolioService.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/PortfolioService.java
@@ -1,10 +1,18 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.portfolios;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.PrimaryTabManager;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.InvestmentGuideline;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Portfolio;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository.PortfolioRepository;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.FieldValidator;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.List;
 
 @Service
@@ -19,5 +27,93 @@ public class PortfolioService {
 
     public void save(Portfolio portfolio) {
         portfolioRepository.save(portfolio);
+    }
+
+    public void savePortfolio(@NonNull Portfolio portfolio, boolean isOnCreate, @NonNull TextField inputPortfolioName,
+                              @NonNull ComboBox<Owner> inputOwner) {
+        portfolio.setName(inputPortfolioName.getText());
+        portfolio.setOwner(inputOwner.getValue());
+        portfolio.setCreatedAt(Calendar.getInstance().getTime());
+        save(portfolio);
+    }
+
+    public boolean isPortfolioInputInvalid(@NonNull TextField inputPortfolioName, @NonNull Portfolio portfolio,
+                                         @NonNull Pane commissionSchemeTablePane, @NonNull Pane commissionSchemeLocationTablePane,
+                                         @NonNull Pane commissionSchemeCurrencyTablePane) {
+        if (FieldValidator.isInputEmpty(inputPortfolioName)) return true;
+
+        float sum_ = 0;
+        for (InvestmentGuideline.Entry entry : portfolio.getInvestmentGuideline().getEntries()) {
+            if (!entry.getType().isChild()) {
+                var assetAlloc = entry.getAssetAllocation();
+                sum_ += assetAlloc;
+
+                float childsSum = 0;
+                for (InvestmentGuideline.Entry child : entry.getChildEntries()) childsSum += child.getAssetAllocation();
+                if (!entry.getChildEntries().isEmpty() && assetAlloc != 0 && childsSum != 100) {
+                    PrimaryTabManager.showDialog(
+                            Alert.AlertType.ERROR,
+                            "Fehler",
+                            String.format("Die Aufteilung des Gesamtvermögens für %s muss in Summe 100 ergeben (Ist: %s).", entry.getType(), childsSum),
+                            (TreeTableView<?>) commissionSchemeTablePane.getChildren().get(0)
+                    );
+                    return true;
+                }
+            }
+        }
+        if (sum_ != 100) {
+            //commissionSchemeTablePane.getChildren().get(0).setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+            PrimaryTabManager.showDialog(
+                    Alert.AlertType.ERROR,
+                    "Fehler",
+                    String.format("Die Aufteilung des Gesamtvermögens muss in Summe 100 ergeben (Ist: %s).", sum_),
+                    (TreeTableView<?>) commissionSchemeTablePane.getChildren().get(0)
+            );
+            return true;
+        }
+
+        var divByLoc = portfolio.getInvestmentGuideline().getDivisionByLocation();
+        float sumDivByLoc = sum(
+                divByLoc.getGermany(), divByLoc.getEurope_without_brd(), divByLoc.getNorthamerica_with_usa(),
+                divByLoc.getAsia_without_china(), divByLoc.getChina(), divByLoc.getJapan(), divByLoc.getEmergine_markets()
+        );
+        if (sumDivByLoc != 100) {
+            //commissionSchemeLocationTablePane.getChildren().get(0).setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+            PrimaryTabManager.showDialog(
+                    Alert.AlertType.ERROR,
+                    "Fehler",
+                    String.format("Die Aufteilung des Gesamtvermögens nach Ländern bzw. Regionen muss in Summe 100 ergeben (Ist: %s).", sumDivByLoc),
+                    (TableView<?>) commissionSchemeLocationTablePane.getChildren().get(0)
+            );
+            return true;
+        }
+
+        var divByCurr = portfolio.getInvestmentGuideline().getDivisionByCurrency();
+        float sumDivByCurr = sum(
+                divByCurr.getEuro(), divByCurr.getUsd(), divByCurr.getGbp(), divByCurr.getYen(),
+                divByCurr.getAsiaCurrencies(), divByCurr.getOthers()
+        );
+        if (sumDivByCurr != 100) {
+            //commissionSchemeCurrencyTablePane.getChildren().get(0).setStyle("-fx-border-color: red; -fx-border-width: 2px;");
+            PrimaryTabManager.showDialog(
+                    Alert.AlertType.ERROR,
+                    "Fehler",
+                    String.format("Die Aufteilung des Gesamtvermögens nach Währung muss in Summe 100 ergeben (Ist: %s).", sumDivByCurr),
+                    (TableView<?>) commissionSchemeCurrencyTablePane.getChildren().get(0)
+            );
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Helper method; sums up the given values.
+     */
+    private float sum(Float... floats) {
+        float sum = 0;
+        for (Float value : floats) {
+            sum += value;
+        }
+        return sum;
     }
 }
