7492e320d65d6a72d721add34e1c03d9d7e785e5
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-20 17:23:49 +0100

    fully implemented

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/portfolio/PortfolioOverviewController.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/portfolio/PortfolioOverviewController.java
@@ -1,13 +1,16 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.portfolios.portfolio;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.PrimaryTabManager;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.PortfolioManagementTabController;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.PortfolioManagementTabManager;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Portfolio;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums.State;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.interfaces.Openable;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners.OwnerService;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.portfolios.PortfolioService;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.InvestmentGuidelineTable;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.PortfolioTreeView;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.view.TableFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -15,6 +18,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class PortfolioOverviewController implements Openable {
@@ -33,6 +38,12 @@ public class PortfolioOverviewController implements Openable {
     @FXML
     ComboBox<Owner> inputOwner;
     @FXML
+    ComboBox<State> inputState;
+    @FXML
+    TextField outputCreatedAt;
+    @FXML
+    TextField outputDeactivatedAt;
+    @FXML
     AnchorPane commissionSchemeTablePane;
     @FXML
     AnchorPane commissionSchemeLocationTablePane;
@@ -49,28 +60,71 @@ public class PortfolioOverviewController implements Openable {
                 .getProperties()
                 .get(PortfolioManagementTabController.TAB_PROPERTY_ENTITY);
 
-        commissionSchemeTablePane.getChildren().add(new InvestmentGuidelineTable(
-                commissionSchemeTablePane,
-                portfolio.getInvestmentGuideline().getEntries()
-        ));
-        commissionSchemeLocationTablePane.getChildren().add(TableFactory.createPortfolioDivisionByLocationTable(
-                commissionSchemeLocationTablePane,
-                portfolio.getInvestmentGuideline().getDivisionByLocation()
-        ));
-        commissionSchemeCurrencyTablePane.getChildren().add(TableFactory.createPortfolioDivisionByCurrencyTable(
-                commissionSchemeCurrencyTablePane,
-                portfolio.getInvestmentGuideline().getDivisionByCurrency()
-        ));
+        loadPortfolioData();
     }
 
     @FXML
     private void onReset() {
+        loadPortfolioData();
     }
 
     @FXML
     private void onSave() {
+        // Validate first
+        if (portfolioService.isPortfolioInputInvalid(
+                inputPortfolioName, portfolio,
+                commissionSchemeTablePane, commissionSchemeLocationTablePane, commissionSchemeCurrencyTablePane
+        )) return;
+
+        // If everything is valid, we can create and save the new portfolio
+        portfolioService.savePortfolio(portfolio, false, inputPortfolioName, inputOwner);
+
+        // Finally, show success-dialog
+        PrimaryTabManager.showInfoDialog(
+                "Portfolio aktualisiert",
+                "Das aktuelle Portfolio wurde erfolgreich aktualisiert.",
+                inputPortfolioName
+        );
     }
 
     private void loadPortfolioData() {
+        inputPortfolioName.setText(portfolio.getName());
+
+        inputOwner.getItems().addAll(ownerService.getAllOwners());
+        inputOwner.getSelectionModel().select(portfolio.getOwner());
+
+        inputState.getItems().addAll(State.values());
+        inputState.getSelectionModel().select(portfolio.getState());
+
+        outputCreatedAt.setText(portfolio.getCreatedAt().toString());
+        outputDeactivatedAt.setText(portfolio.getDeactivatedAt() != null ? portfolio.getDeactivatedAt().toString() : "");
+
+        commissionSchemeTablePane.getChildren().clear();
+        commissionSchemeTablePane.getChildren().add(new InvestmentGuidelineTable(
+                commissionSchemeTablePane,
+                portfolio.getInvestmentGuideline().getEntries()
+        ));
+
+        commissionSchemeLocationTablePane.getChildren().clear();
+        commissionSchemeLocationTablePane.getChildren().add(TableFactory.createPortfolioDivisionByLocationTable(
+                commissionSchemeLocationTablePane,
+                portfolio.getInvestmentGuideline().getDivisionByLocation()
+        ));
+
+        commissionSchemeCurrencyTablePane.getChildren().clear();
+        commissionSchemeCurrencyTablePane.getChildren().add(TableFactory.createPortfolioDivisionByCurrencyTable(
+                commissionSchemeCurrencyTablePane,
+                portfolio.getInvestmentGuideline().getDivisionByCurrency()
+        ));
+
+        var portfolioTreeView = new PortfolioTreeView(
+                portfolioTreeViewPane,
+                List.of(portfolio),
+                portfolioManagementManager,
+                false
+        );
+        portfolioTreeView.setShowRoot(false);
+        portfolioTreeViewPane.getChildren().clear();
+        portfolioTreeViewPane.getChildren().add(portfolioTreeView);
     }
 }
