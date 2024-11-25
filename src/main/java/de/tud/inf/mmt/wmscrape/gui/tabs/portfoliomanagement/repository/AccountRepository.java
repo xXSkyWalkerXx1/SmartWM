5dc37381b78a5526318284948b7b0dcc935b052d
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-25 15:42:50 +0100

    fixed bean-name

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/AccountRepository.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository;
 
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("pAccountRepository")
 public interface AccountRepository extends JpaRepository<Account, String> {
 }
