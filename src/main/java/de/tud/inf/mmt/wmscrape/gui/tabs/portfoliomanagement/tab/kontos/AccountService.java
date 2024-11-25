b86731bc9c81311904957344d321bb4bd9c34790
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-25 18:18:34 +0100

    implemented method 'writeInput'

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/kontos/AccountService.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/kontos/AccountService.java
@@ -1,10 +1,21 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.kontos;
 
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Account;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Portfolio;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums.AccountType;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums.InterestInterval;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository.AccountRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Currency;
 import java.util.List;
 
 @Service
@@ -19,5 +30,28 @@ public class AccountService {
 
     public void save(Account account) {
         accountRepository.save(account);
+    }
+
+    public void writeInput(@NonNull Account account, boolean isOnCreate,
+                           @NonNull TextField inputDescription, @NonNull ComboBox<AccountType> inputType,
+                           @NonNull ComboBox<Currency> inputCurrencyCode, @NonNull TextField inputBalance,
+                           @NonNull ComboBox<Owner> inputOwner, @NonNull ComboBox<Portfolio> inputPortfolio,
+                           @NonNull TextArea inputNotice, @NonNull TextField inputBankName, @NonNull TextField inputIban,
+                           @NonNull TextField inputKontoNumber, @NonNull TextField inputInterestRate,
+                           @NonNull TextField inputInterestDays, @NonNull ComboBox<InterestInterval> inputInterestInterval) {
+        account.setDescription(inputDescription.getText());
+        account.setType(inputType.getValue());
+        account.setCurrencyCode(inputCurrencyCode.getValue());
+        account.setBalance(Float.parseFloat(inputBalance.getText()));
+        account.setOwner(inputOwner.getValue());
+        account.setPortfolio(inputPortfolio.getValue());
+        account.setNotice(inputNotice.getText());
+        if (isOnCreate) account.setCreatedAt(Calendar.getInstance().getTime());
+        account.setBankName(inputBankName.getText());
+        account.setIban(inputIban.getText());
+        account.setKontoNumber(inputKontoNumber.getText());
+        account.setInterestRate(Float.parseFloat(inputInterestRate.getText()));
+        account.setInterestDays(inputInterestDays.getText());
+        account.setInterestInterval(inputInterestInterval.getValue());
     }
 }
