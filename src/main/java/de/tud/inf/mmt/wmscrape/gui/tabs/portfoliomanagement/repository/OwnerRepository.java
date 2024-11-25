b1a0bf1312b4a76df379dc3c5e935bb3b71ecea9
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-11-25 15:22:42 +0100

    refactored

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/OwnerRepository.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/OwnerRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface OwnerRepository extends JpaRepository<Owner, Integer> {
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
 }
