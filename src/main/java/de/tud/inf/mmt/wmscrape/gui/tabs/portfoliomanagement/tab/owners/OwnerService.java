fdb462ebcfa9e9e78c7a8f546c125941d3d66c17
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-12-04 11:39:37 +0100

    implemented method 'delete'

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/OwnerService.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/owners/OwnerService.java
@@ -1,11 +1,10 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.owners;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.PrimaryTabManager;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums.MaritalState;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository.OwnerRepository;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -25,6 +24,21 @@ public class OwnerService {
 
     public void save(Owner owner) {
         ownerRepository.save(owner);
+    }
+
+    /**
+     * @param control Some element inside the controller class used as a reference to get the stage/ scene.
+     */
+    public void delete(Owner owner, @NonNull Control control) {
+        PrimaryTabManager.showDialogWithAction(
+                Alert.AlertType.WARNING,
+                "Inhaber löschen",
+                "Sind Sie sicher, dass Sie den Inhaber löschen möchten?\n" +
+                        "Etwaige Beziehungen zu Konten oder Depots werden dabei nicht berücksichtig und kann zu einem " +
+                        "fehlerhaften Verhalten der Anwendung führen!;",
+                control,
+                o -> ownerRepository.delete(owner)
+        );
     }
 
     public void writeInput(@NonNull Owner owner, boolean isOnCreate,
@@ -50,7 +64,7 @@ public class OwnerService {
         ownerTaxInfo.setTaxNumber(inputTaxNumber.getText());
         ownerTaxInfo.setMaritalState(inputMaritalState.getValue());
         ownerTaxInfo.setTaxRate(Double.parseDouble(inputTaxRate.getText()));
-        ownerTaxInfo.setChurchTaxRate(Double.parseDouble(inputChurchTaxRate.getText() == "" ? "0" : inputChurchTaxRate.getText()));
+        ownerTaxInfo.setChurchTaxRate(Double.parseDouble(inputChurchTaxRate.getText().isBlank() ? "0" : inputChurchTaxRate.getText()));
         ownerTaxInfo.setCapitalGainsTaxRate(Double.parseDouble(inputCapitalGainsTaxRate.getText()));
         ownerTaxInfo.setSolidaritySurchargeTaxRate(Double.parseDouble(inputSolidaritySurchargeTaxRate.getText()));
     }
