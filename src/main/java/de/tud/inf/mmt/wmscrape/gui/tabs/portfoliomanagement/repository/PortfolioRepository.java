da48dbb08c804ae21d8c6c09dd0398a8eb4e5c1a
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-12-06 11:31:40 +0100

    changed type of id

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/PortfolioRepository.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/PortfolioRepository.java
@@ -3,6 +3,6 @@ package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.repository;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PortfolioRepository extends JpaRepository<Portfolio, String> {
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 
 }
