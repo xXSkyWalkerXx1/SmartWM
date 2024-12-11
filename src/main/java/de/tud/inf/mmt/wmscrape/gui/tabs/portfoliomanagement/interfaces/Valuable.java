f9fb8366b8f6ea11e5755cc097bdd7bf73dbddfa
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-12-11 12:34:24 +0100

    implemented method 'getValueCurrency', added param to method 'getValue' - also this can throw now a DataAccessException

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/interfaces/Valuable.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/interfaces/Valuable.java
@@ -1,11 +1,23 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.interfaces;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.tab.kontos.AccountService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.lang.NonNull;
+
 import java.math.BigDecimal;
+import java.util.Currency;
 
 /**
  * Represents an entity (f.e. bank account) with a monetary value.
  */
 public interface Valuable {
 
-    BigDecimal getValue();
+    /**
+     * @param accountService the account service to use for converting in EUR.
+     * @return the value of this entity in EUR.
+     * @throws DataAccessException if the value could not be converted to EUR (f.e. no exchange-entry in table for the given currency).
+     */
+    BigDecimal getValue(@NonNull AccountService accountService) throws DataAccessException;
+
+    Currency getValueCurrency();
 }
