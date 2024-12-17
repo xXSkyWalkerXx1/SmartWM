9cdc258db7575fbef470541b5ebfcaa9b8bc2f8c
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-12-17 12:44:41 +0100

    implemented 2 new methods, added javadocs

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/AccountRepository.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/AccountRepository.java
@@ -14,12 +14,34 @@ import java.util.Optional;
 @Repository("pAccountRepository")
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+    /**
+     * @return all accounts where the owner or the portfolio is null or invalid.
+     */
     @Query(value = "SELECT a.id " +
             "FROM pkonto a " +
             "LEFT JOIN inhaber o ON a.owner_id = o.id " +
             "LEFT JOIN portfolio p ON a.portfolio_id = p.id " +
             "WHERE o.id IS NULL OR p.id IS NULL OR a.owner_id IS NULL OR a.portfolio_id IS NULL", nativeQuery = true)
     List<Long> findAllByOwnerAndPortfolioIsInvalid();
+
+    /**
+     * @return all accounts where the currency code, balance, bank name, konto number, interest rate, iban or the created_at date is null.
+     */
+    @Query("SELECT a.id " +
+            "FROM pAccount a " +
+            "WHERE a.currencyCode IS NULL OR a.balance IS NULL OR a.bankName IS NULL OR a.kontoNumber IS NULL " +
+            "OR a.interestRate IS NULL OR a.iban IS NULL OR a.createdAt IS NULL")
+    List<Long> findAllByNullEntry();
+
+    /**
+     * @return all accounts with an invalid state, type or interest interval.
+     */
+    @Query("SELECT a.id " +
+            "FROM pAccount a " +
+            "WHERE a.state != 'ACTIVATED' AND a.state != 'DEACTIVATED' " +
+            "OR a.type != 'CLEARING_ACCOUNT' AND a.type != 'CHECKING_ACCOUNT' AND a.type != 'FIXED_TERM_DEPOSIT_ACCOUNT' " +
+            "OR a.interestInterval != 'MONTHLY' AND a.interestInterval != 'QUARTERLY' AND a.interestInterval != 'YEARLY'")
+    List<Long> findAllByInvalidLiteral();
 
     @Query("SELECT a.iban FROM pAccount a WHERE a.id = :id")
     Optional<String> findIbanBy(Long id);
