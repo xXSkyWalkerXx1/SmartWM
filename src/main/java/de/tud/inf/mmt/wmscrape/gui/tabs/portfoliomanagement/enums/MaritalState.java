8e4817f5bf92068879f2b4688c2ea9524b039a79
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-12-19 21:16:50 +0100

    implemented method to get all values as string

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/enums/MaritalState.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/enums/MaritalState.java
@@ -1,5 +1,9 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum MaritalState {
     SINGLE("Ledig"),
     MARRIED("Verheiratet"),
@@ -18,5 +22,12 @@ public enum MaritalState {
     @Override
     public String toString() {
         return displayText;
+    }
+
+    /**
+     * Returns the enum value as a string, using {@code Enum.name()}.
+     */
+    public static List<String> getValuesAsString() {
+        return Arrays.stream(MaritalState.values()).map(MaritalState::name).collect(Collectors.toList());
     }
 }
--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/enums/State.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/enums/State.java
@@ -1,5 +1,9 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum State {
     ACTIVATED("Aktiviert"),
     DEACTIVATED("Deaktiviert");
@@ -17,5 +21,12 @@ public enum State {
     @Override
     public String toString() {
         return displayText;
+    }
+
+    /**
+     * Returns the enum value as a string, using {@code Enum.name()}.
+     */
+    public static List<String> getValuesAsString() {
+        return Arrays.stream(State.values()).map(State::name).collect(Collectors.toList());
     }
 }
