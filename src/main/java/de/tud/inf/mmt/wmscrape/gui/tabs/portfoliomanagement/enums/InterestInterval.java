495d5d8d4bf233fabf38cb468144ecccae728f8b
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-12-23 13:29:43 +0100

    implemented method 'getValuesAsString'

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/enums/InterestInterval.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/enums/InterestInterval.java
@@ -1,5 +1,9 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum InterestInterval {
     MONTHLY("Monatlich"),
     QUARTERLY("Quartalsweise"),
@@ -14,5 +18,12 @@ public enum InterestInterval {
     @Override
     public String toString() {
         return displayText;
+    }
+
+    /**
+     * Returns the enum value as a string, using {@code Enum.name()}.
+     */
+    public static List<String> getValuesAsString() {
+        return Arrays.stream(InterestInterval.values()).map(InterestInterval::name).collect(Collectors.toList());
     }
 }
