22465c127ad934b16dd15f9e9daadaac83516aa0
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-12-23 14:05:12 +0100

    reimplemented methods to check for inconsistencies

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/AccountRepository.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/repository/AccountRepository.java
@@ -8,12 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Repository("pAccountRepository")
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+    @Query(value = "SELECT a.id FROM pkonto a", nativeQuery = true)
+    List<Long> getAllIds();
+
+    // region Queries to check for inconsistencies
     /**
      * @return all accounts where the owner or the portfolio is null or invalid.
      */
@@ -27,23 +32,59 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     /**
      * @return all accounts where the currency code, balance, bank name, konto number, interest rate, iban or the created_at date is null.
      */
-    @Query("SELECT a.id " +
-            "FROM pAccount a " +
-            "WHERE a.currencyCode IS NULL OR a.balance IS NULL OR a.bankName IS NULL OR a.kontoNumber IS NULL " +
-            "OR a.interestRate IS NULL OR a.iban IS NULL OR a.createdAt IS NULL")
-    List<Long> findAllByNullEntry();
+    @Query(value = "SELECT a.id " +
+            "FROM pkonto a " +
+            "WHERE a.currency_code IS NULL OR a.balance IS NULL OR a.bank_name IS NULL OR a.konto_number IS NULL " +
+            "OR a.interest_rate IS NULL OR a.iban IS NULL OR a.created_at IS NULL", nativeQuery = true)
+    List<Long> findByCurrencyCodeIsNullOrBalanceIsNullOrBankNameIsNullOrKontoNumberIsNullOrInterestRateIsNullOrIbanIsNullOrCreatedAtIsNull();
 
     /**
-     * @return all accounts with an invalid state, type or interest interval.
+     * @param state pass always {@code State.getValuesAsString()}.
+     * @param type pass always {@code AccountType.values()}.
+     * @param interestInterval pass always {@code InterestInterval.values()}.
+     * @return all accounts where the state, type or interest interval is not mappable to a literal of the enum.
      */
-    @Query("SELECT a.id " +
-            "FROM pAccount a " +
-            "WHERE a.state != 'ACTIVATED' AND a.state != 'DEACTIVATED' " +
-            "OR a.type != 'CLEARING_ACCOUNT' AND a.type != 'CHECKING_ACCOUNT' AND a.type != 'FIXED_TERM_DEPOSIT_ACCOUNT' " +
-            "OR a.interestInterval != 'MONTHLY' AND a.interestInterval != 'QUARTERLY' AND a.interestInterval != 'YEARLY'")
-    List<Long> findAllByInvalidLiteral();
+    @Query(value = "SELECT a.id " +
+            "FROM pkonto a " +
+            "WHERE a.state NOT IN :state " +
+            "OR a.type NOT IN :type " +
+            "OR a.interest_interval NOT IN :interestInterval", nativeQuery = true)
+    List<Long> findByStateNotInOrTypeNotInOrInterestIntervalNotIn(Collection<String> state,
+                                                                  Collection<String> type,
+                                                                  Collection<String> interestInterval);
 
-    @Query("SELECT a.iban FROM pAccount a WHERE a.id = :id")
+    /**
+     * @return all accounts where the interest rate is null or not between 0 and 100.
+     */
+    @Query(value = "SELECT a.id " +
+            "FROM pkonto a " +
+            "WHERE a.interest_rate IS NULL OR a.interest_rate NOT BETWEEN 0 AND 100", nativeQuery = true)
+    List<Long> findByInterestRateIsNotBetween0And100();
+
+    /**
+     * @return all accounts where interest days is null or not between 0 and 365.
+     */
+    @Query(value = "SELECT a.id " +
+            "FROM pkonto a " +
+            "WHERE a.interest_days IS NULL OR a.interest_days NOT BETWEEN 0 AND 365", nativeQuery = true)
+    List<Long> findByInterestDaysIsNotBetween0And365();
+
+    /**
+     * @param currencies pass always {@code Currency.getAvailableCurrencies()}.
+     * @return Accounts with an invalid currency.
+     */
+    @Query(value = "SELECT a.id " +
+            "FROM pkonto a " +
+            "WHERE a.currency_code IS NULL OR a.currency_code NOT IN :currencies", nativeQuery = true)
+    List<Long> findByCurrencyIsNotIn(Collection<String> currencies);
+    // endregion
+
+
+
+    @Query(value = "SELECT a.currency_code FROM pkonto a WHERE a.id = :id", nativeQuery = true)
+    Optional<String> findCurrencyCodeBy(Long id);
+
+    @Query(value = "SELECT a.iban FROM pkonto a WHERE a.id = :id", nativeQuery = true)
     Optional<String> findIbanBy(Long id);
 
     @Transactional
