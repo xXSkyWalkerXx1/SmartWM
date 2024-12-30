64902a4cfd0fbb2f1f6f21c9ac6c792186f52680
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2024-12-30 16:38:26 +0100

    implemented loading data of reconstructed portfolio

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/dialog/FixPortfolioInconsistenciesDialog.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/tab/portfolios/dialog/FixPortfolioInconsistenciesDialog.java
@@ -7,6 +7,10 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
 @Controller
 public class FixPortfolioInconsistenciesDialog extends CreatePortfolioDialog {
 
@@ -19,10 +23,49 @@ public class FixPortfolioInconsistenciesDialog extends CreatePortfolioDialog {
 
     @Override
     protected void initialize() {
-        if (portfolio == null || portfolio.getId() == null) {
-            throw new IllegalStateException("Portfolio must be set before initializing dialog.");
-        }
+        if (portfolio == null) throw new IllegalStateException("Portfolio must be set before initializing dialog.");
         super.initialize();
+
+        // Set content of combo-boxes
+        inputOwner.getItems().setAll(ownerService.getOwnerRepository().findAllAsFake());
+
+        inputState.getItems().setAll(State.values());
+        inputState.getSelectionModel().selectedItemProperty().addListener((observableValue, state, t1) -> {
+            if (t1 == State.DEACTIVATED) {
+                inputDeactivatedAt.setDisable(false);
+            } else if (t1 == State.ACTIVATED) {
+                inputDeactivatedAt.setDisable(true);
+                inputDeactivatedAt.setValue(null);
+            }
+        });
+
+        // Set the owner's information
+        inputPortfolioName.setText(portfolio.getName());
+        inputOwner.getSelectionModel().select(portfolio.getOwner());
+        inputState.getSelectionModel().select(portfolio.getState());
+
+        if (portfolio.getCreatedAt() != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(portfolio.getCreatedAt());
+            inputCreatedAt.setValue(LocalDate.of(
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH) + 1,
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            ));
+            inputCreatedAt.getEditor().setText(inputCreatedAt.getValue().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        }
+
+        if (portfolio.getDeactivatedAt() != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(portfolio.getDeactivatedAt());
+            inputDeactivatedAt.setValue(LocalDate.of(
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH) + 1,
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            ));
+            inputDeactivatedAt.getEditor().setText(inputDeactivatedAt.getValue().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        }
+
     }
 
     public void setPortfolio(Portfolio portfolio) {
