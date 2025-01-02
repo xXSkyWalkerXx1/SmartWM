fbba0d8218003382d857af1ec32b0c596a8473fe
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2025-01-02 10:47:01 +0100

    changed type of 'element' from String to Object

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/BreadcrumbElement.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/BreadcrumbElement.java
@@ -1,10 +1,12 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement;
 
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.FinancialAsset;
+
 public class BreadcrumbElement{
-    public String element;
+    public Object element;
     public String type;
 
-    public BreadcrumbElement(String element, String type) {
+    public BreadcrumbElement(Object element, String type) {
         this.element = element;
         this.type = type;
     }
