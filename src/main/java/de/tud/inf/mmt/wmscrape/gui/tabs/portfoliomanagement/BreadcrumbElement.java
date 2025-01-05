ac07a40b31b683745614eb8e0b8a35978d339f96
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2025-01-05 15:00:26 +0100

    type is now represented by enum instead of string

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/BreadcrumbElement.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/BreadcrumbElement.java
@@ -1,12 +1,13 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement;
 
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.FinancialAsset;
+import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums.BreadcrumbElementType;
 
 public class BreadcrumbElement{
     public Object element;
-    public String type;
+    public BreadcrumbElementType type;
 
-    public BreadcrumbElement(Object element, String type) {
+    public BreadcrumbElement(Object element, BreadcrumbElementType type) {
         this.element = element;
         this.type = type;
     }
