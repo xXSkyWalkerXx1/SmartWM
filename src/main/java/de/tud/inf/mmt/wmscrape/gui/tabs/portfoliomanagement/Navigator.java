404e0e3bb15cd79550d4ac76459e16b62eb436cb
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2025-01-07 16:34:13 +0100

    changed type of 'isFirstBreadcrumb' from primitive-type boolean to class Boolean

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/Navigator.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/Navigator.java
@@ -6,16 +6,18 @@ import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Owner;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.Portfolio;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums.BreadcrumbElementType;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 public class Navigator {
 
     /**
      * Navigates to overview of specific owner.
-     * @param isFirstBreadcrumb if true, the owner will be the first element in the breadcrumb otherwise it will be added to the end.
+     * @param isFirstBreadcrumb if true, the owner will be the first element in the breadcrumb, if false it will be
+     *                          added to the end, otherwise if null no action will be performed.
      */
     public static void navigateToOwner(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
                                        @NonNull Owner owner,
-                                       boolean isFirstBreadcrumb) {
+                                       @Nullable Boolean isFirstBreadcrumb) {
         portfolioManagementTabManager.showInhaberTabs(owner);
         setOrAddBreadcrumb(portfolioManagementTabManager, isFirstBreadcrumb, owner, BreadcrumbElementType.OWNER);
     }
@@ -36,22 +38,24 @@ public class Navigator {
 
     /**
      * Navigates to overview of specific portfolio.
-     * @param isFirstBreadcrumb if true, the portfolio will be the first element in the breadcrumb otherwise it will be added to the end.
+     * @param isFirstBreadcrumb  if true, the portfolio will be the first element in the breadcrumb, if false it will be
+     *                           added to the end, otherwise if null no action will be performed.
      */
     public static void navigateToPortfolio(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
                                            @NonNull Portfolio portfolio,
-                                           @NonNull boolean isFirstBreadcrumb) {
+                                           @Nullable Boolean isFirstBreadcrumb) {
         portfolioManagementTabManager.showPortfolioTabs(portfolio);
         setOrAddBreadcrumb(portfolioManagementTabManager, isFirstBreadcrumb, portfolio, BreadcrumbElementType.PORTFOLIO);
     }
 
     /**
      * Navigates to overview of specific account.
-     * @param isFirstBreadcrumb if true, the account will be the first element in the breadcrumb otherwise it will be added to the end.
+     * @param isFirstBreadcrumb  if true, the account will be the first element in the breadcrumb, if false it will be
+     *                           added to the end, otherwise if null no action will be performed.
      */
     public static void navigateToAccount(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
                                          @NonNull Account account,
-                                         boolean isFirstBreadcrumb) {
+                                         @Nullable Boolean isFirstBreadcrumb) {
         portfolioManagementTabManager.showKontoTabs(account);
         setOrAddBreadcrumb(portfolioManagementTabManager, isFirstBreadcrumb, account, BreadcrumbElementType.ACCOUNT);
     }
@@ -72,24 +76,28 @@ public class Navigator {
 
     /**
      * Navigates to overview of specific depot.
-     * @param isFirstBreadcrumb if true, the depot will be the first element in the breadcrumb otherwise it will be added to the end.
+     * @param isFirstBreadcrumb  if true, the depot will be the first element in the breadcrumb, if false it will be
+     *      *                    added to the end, otherwise if null no action will be performed.
      */
     public static void navigateToDepot(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
                                        @NonNull Depot depot,
-                                       boolean isFirstBreadcrumb) {
+                                       @Nullable Boolean isFirstBreadcrumb) {
         portfolioManagementTabManager.showDepotTabs();
         setOrAddBreadcrumb(portfolioManagementTabManager, isFirstBreadcrumb, depot, BreadcrumbElementType.DEPOT);
     }
 
     /**
-     * @param isFirstBreadcrumb if true, the breadcrumb-element will be the first element in the breadcrumb otherwise it will be added to the end.
+     * @param isFirstBreadcrumb if true, the breadcrumb-element will be the first element in the breadcrumb, if
+     *                          false it will be added to the end and if null no action will be performed
      * @param breadcrumbElement see {@link BreadcrumbElement#element}
      * @param type see {@link BreadcrumbElement#type} (can be {@code konto}, {@code depot}, {@code portfolio} or {@code owner})
      */
     private static void setOrAddBreadcrumb(@NonNull PortfolioManagementTabManager portfolioManagementTabManager,
-                                    boolean isFirstBreadcrumb,
-                                    @NonNull Object breadcrumbElement,
-                                    @NonNull BreadcrumbElementType type) {
+                                           @Nullable Boolean isFirstBreadcrumb,
+                                           @NonNull Object breadcrumbElement,
+                                           @NonNull BreadcrumbElementType type) {
+        if (isFirstBreadcrumb == null) return;
+
         if (isFirstBreadcrumb) {
             portfolioManagementTabManager.setCurrentlyDisplayedElement(new BreadcrumbElement(breadcrumbElement, type));
         } else {
