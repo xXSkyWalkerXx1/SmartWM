fd57c350bcc298036099d9d2cfea84ec8b27862e
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2025-02-18 22:57:19 +0100

    implemented 'toString'

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/BreadcrumbElement.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/BreadcrumbElement.java
@@ -1,15 +1,21 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement;
 
-import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.entity.FinancialAsset;
 import de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.enums.BreadcrumbElementType;
+import org.springframework.lang.NonNull;
 
-public class BreadcrumbElement{
+public class BreadcrumbElement {
+
     public Object element;
-    public BreadcrumbElementType type;
+    public final BreadcrumbElementType type;
 
-    public BreadcrumbElement(Object element, BreadcrumbElementType type) {
+    public BreadcrumbElement(@NonNull Object element, @NonNull BreadcrumbElementType type) {
         this.element = element;
         this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
     }
 }
 
