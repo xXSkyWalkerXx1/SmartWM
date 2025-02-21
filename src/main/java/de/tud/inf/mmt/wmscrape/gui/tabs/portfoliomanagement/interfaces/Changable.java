9effe81fe9d853b74f17df6483e51de141500ed6
xxskywalkerxx1 <philipp.jonscher@mailbox.tu-dresden.de>
2025-02-21 20:22:21 +0100

    reimplemented

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/interfaces/Changable.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/portfoliomanagement/interfaces/Changable.java
@@ -1,8 +1,15 @@
 package de.tud.inf.mmt.wmscrape.gui.tabs.portfoliomanagement.interfaces;
 
+import javax.persistence.PostLoad;
+
 public interface Changable {
 
     boolean isChanged();
 
-    void setChanged(boolean changed);
+    void restore();
+
+    void onPrePersistOrUpdateOrRemoveEntity();
+
+    @PostLoad
+    void onPostLoadEntity();
 }
